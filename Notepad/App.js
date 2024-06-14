6044c509c810c0d1bc79a6ac8d646bb058813f3d
Jose Almir <joseamfj@outlook.com>
2024-06-14 11:40:00 -0300

    Create folders structure

--- Notepad/App.js
+++ Notepad/App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
@@ -8,10 +8,24 @@ import AppLoading from 'expo-app-loading';
 
 const Stack = createStackNavigator();
 
+const Folder = ({ name, onPress }) => {
+    return (
+        <TouchableOpacity style={styles.folder} onPress={onPress}>
+            <Text style={styles.folderText}>{name}</Text>
+        </TouchableOpacity>
+    );
+};
+
 const HomeScreen = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>NiN</Text>
+            <ScrollView contentContainerStyle={styles.foldersContainer}>
+                <Folder name="Geral" onPress={() => { }} />
+                <Folder name="Geral" onPress={() => { }} />
+                <Folder name="Geral" onPress={() => { }} />
+
+            </ScrollView>
         </View>
     );
 };
@@ -44,7 +58,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'center',
         backgroundColor: '#f0f0f0',
-        paddingTop: 100,
+        paddingTop: 65,
     },
     title: {
         fontSize: 60,
@@ -53,5 +67,29 @@ const styles = StyleSheet.create({
         textShadowColor: '#aaa',
         textShadowOffset: { width: 2, height: 2 },
         textShadowRadius: 5,
+        marginBottom: 20,
+    },
+    foldersContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    folder: {
+        width: 250,
+        height: 75,
+        backgroundColor: '#e0e0e0',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+    },
+    folderText: {
+        fontSize: 18,
+        color: '#333',
     },
 });
